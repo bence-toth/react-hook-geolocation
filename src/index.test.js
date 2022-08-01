56145bcdedd05bdc20708af8791c0f70284a2d14
Bence A. Tóth <tothab@gmail.com>
2022-08-02 00:24:32 +0200

    Extended and restructured tests

--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,7 @@ import { renderHook } from "@testing-library/react";
 const mockGetCurrentPosition = jest.fn();
 const mockWatchPosition = jest.fn();
 const mockClearWatch = jest.fn();
+const mockCallback = jest.fn();
 
 const mockGeolocation = {
   getCurrentPosition: mockGetCurrentPosition,
@@ -23,159 +24,210 @@ const errorHandler = (data) => (_, onError) => {
 };
 
 describe("useGeolocation", () => {
-  it("reads initial geolocation", () => {
-    const mockCoordinates = {
-      latitude: 12.3456789,
-      longitude: 34.5678912,
-      altitude: null,
-      accuracy: 12.345,
-      altitudeAccuracy: null,
-      heading: null,
-      speed: null,
-    };
-    mockGetCurrentPosition.mockImplementationOnce(
-      successHandler({ coords: mockCoordinates })
-    );
-
-    const { result } = renderHook(() => useGeolocation());
-
-    expect(result.current).toStrictEqual({
-      ...mockCoordinates,
-      timestamp: undefined,
-      error: null,
+  describe("when geolocation data is available", () => {
+    beforeEach(() => {
+      mockCallback.mockReset();
+      mockClearWatch.mockReset();
     });
-  });
 
-  it("updates geolocation from watcher", () => {
-    const mockCoordinatesInitial = {
-      latitude: 12.3456789,
-      longitude: 34.5678912,
-      altitude: null,
-      accuracy: 12.345,
-      altitudeAccuracy: null,
-      heading: null,
-      speed: null,
-    };
-    mockGetCurrentPosition.mockImplementationOnce(
-      successHandler({ coords: mockCoordinatesInitial })
-    );
+    it("reads initial geolocation", () => {
+      const mockCoordinates = {
+        latitude: 12.3456789,
+        longitude: 34.5678912,
+        altitude: null,
+        accuracy: 12.345,
+        altitudeAccuracy: null,
+        heading: null,
+        speed: null,
+      };
+      mockGetCurrentPosition.mockImplementationOnce(
+        successHandler({ coords: mockCoordinates })
+      );
 
-    const mockCoordinatesWatch = {
-      latitude: 23.4567891,
-      longitude: 45.6789123,
-      altitude: null,
-      accuracy: 2.3456,
-      altitudeAccuracy: null,
-      heading: null,
-      speed: null,
-    };
-    const mockTimestamp = 1659386826480;
-    mockWatchPosition.mockImplementationOnce(
-      successHandler({
-        coords: mockCoordinatesWatch,
-        timestamp: mockTimestamp,
-      })
-    );
+      const { result } = renderHook(() => useGeolocation({}, mockCallback));
 
-    const { result } = renderHook(() => useGeolocation());
+      expect(result.current).toStrictEqual({
+        ...mockCoordinates,
+        timestamp: undefined,
+        error: null,
+      });
 
-    expect(result.current).toStrictEqual({
-      ...mockCoordinatesWatch,
-      timestamp: mockTimestamp,
-      error: null,
+      requestAnimationFrame(() => {
+        expect(mockCallback).toHaveBeenCalledWith({
+          ...mockCoordinates,
+          timestamp: undefined,
+          error: null,
+        });
+      });
     });
-  });
 
-  it("handles initial error", () => {
-    const mockError = {
-      code: 1,
-      message: "User denied Geolocation",
-    };
-    mockGetCurrentPosition.mockImplementationOnce(errorHandler(mockError));
+    it("updates geolocation from watch", () => {
+      const mockCoordinatesInitial = {
+        latitude: 12.3456789,
+        longitude: 34.5678912,
+        altitude: null,
+        accuracy: 12.345,
+        altitudeAccuracy: null,
+        heading: null,
+        speed: null,
+      };
+      mockGetCurrentPosition.mockImplementationOnce(
+        successHandler({ coords: mockCoordinatesInitial })
+      );
 
-    const { result } = renderHook(() => useGeolocation());
-
-    expect(result.current).toStrictEqual({
-      accuracy: null,
-      altitude: null,
-      altitudeAccuracy: null,
-      heading: null,
-      latitude: null,
-      longitude: null,
-      speed: null,
-      timestamp: null,
-      error: mockError,
-    });
-  });
-
-  it("handles error from watcher", () => {
-    const mockCoordinates = {
-      latitude: 12.3456789,
-      longitude: 34.5678912,
-      altitude: null,
-      accuracy: 12.345,
-      altitudeAccuracy: null,
-      heading: null,
-      speed: null,
-    };
-    mockGetCurrentPosition.mockImplementationOnce(
-      successHandler({ coords: mockCoordinates })
-    );
-
-    const mockError = {
-      code: 1,
-      message: "User denied Geolocation",
-    };
-    mockWatchPosition.mockImplementationOnce(errorHandler(mockError));
-
-    const { result } = renderHook(() => useGeolocation());
-
-    expect(result.current).toStrictEqual({
-      accuracy: null,
-      altitude: null,
-      altitudeAccuracy: null,
-      heading: null,
-      latitude: null,
-      longitude: null,
-      speed: null,
-      timestamp: null,
-      error: mockError,
-    });
-  });
-
-  it("clears watch", async () => {
-    mockClearWatch.mockReset();
-
-    const mockCoordinatesInitial = {
-      latitude: 12.3456789,
-      longitude: 34.5678912,
-    };
-    mockGetCurrentPosition.mockImplementationOnce(
-      successHandler({
-        coords: mockCoordinatesInitial,
-      })
-    );
-
-    const mockCoordinatesWatch = {
-      latitude: 23.4567891,
-      longitude: 45.6789123,
-    };
-    const mockTimestamp = 1659386826480;
-    const mockWatchId = 987;
-    mockWatchPosition.mockImplementationOnce(
-      successHandler(
-        {
+      const mockCoordinatesWatch = {
+        latitude: 23.4567891,
+        longitude: 45.6789123,
+        altitude: null,
+        accuracy: 2.3456,
+        altitudeAccuracy: null,
+        heading: null,
+        speed: null,
+      };
+      const mockTimestamp = 1659386826480;
+      mockWatchPosition.mockImplementationOnce(
+        successHandler({
           coords: mockCoordinatesWatch,
           timestamp: mockTimestamp,
-        },
-        mockWatchId
-      )
-    );
+        })
+      );
 
-    renderHook(() => useGeolocation());
+      const { result } = renderHook(() => useGeolocation({}, mockCallback));
 
-    requestAnimationFrame(() => {
-      expect(mockClearWatch).toHaveBeenCalledWith(mockWatchId);
+      expect(result.current).toStrictEqual({
+        ...mockCoordinatesWatch,
+        timestamp: mockTimestamp,
+        error: null,
+      });
+
+      requestAnimationFrame(() => {
+        expect(mockCallback).toHaveBeenCalledTimes(2);
+        expect(mockCallback).toHaveBeenCalledWith({
+          ...mockCoordinatesInitial,
+          timestamp: undefined,
+          error: null,
+        });
+        expect(mockCallback).toHaveBeenCalledWith({
+          ...mockCoordinatesWatch,
+          timestamp: mockTimestamp,
+          error: null,
+        });
+      });
+    });
+
+    it("clears watch", () => {
+      const mockCoordinatesInitial = {
+        latitude: 12.3456789,
+        longitude: 34.5678912,
+      };
+      mockGetCurrentPosition.mockImplementationOnce(
+        successHandler({
+          coords: mockCoordinatesInitial,
+        })
+      );
+
+      const mockCoordinatesWatch = {
+        latitude: 23.4567891,
+        longitude: 45.6789123,
+      };
+      const mockTimestamp = 1659386826480;
+      const mockWatchId = 987;
+      mockWatchPosition.mockImplementationOnce(
+        successHandler(
+          {
+            coords: mockCoordinatesWatch,
+            timestamp: mockTimestamp,
+          },
+          mockWatchId
+        )
+      );
+
+      renderHook(() => useGeolocation());
+
+      requestAnimationFrame(() => {
+        expect(mockClearWatch).toHaveBeenCalledWith(mockWatchId);
+      });
+    });
+  });
+
+  describe("when geolocation data is not available", () => {
+    it("handles initial error", () => {
+      const mockError = {
+        code: 1,
+        message: "User denied Geolocation",
+      };
+      mockGetCurrentPosition.mockImplementationOnce(errorHandler(mockError));
+
+      const { result } = renderHook(() => useGeolocation());
+
+      expect(result.current).toStrictEqual({
+        accuracy: null,
+        altitude: null,
+        altitudeAccuracy: null,
+        heading: null,
+        latitude: null,
+        longitude: null,
+        speed: null,
+        timestamp: null,
+        error: mockError,
+      });
+    });
+
+    it("handles error from watcher", () => {
+      const mockCoordinates = {
+        latitude: 12.3456789,
+        longitude: 34.5678912,
+        altitude: null,
+        accuracy: 12.345,
+        altitudeAccuracy: null,
+        heading: null,
+        speed: null,
+      };
+      mockGetCurrentPosition.mockImplementationOnce(
+        successHandler({ coords: mockCoordinates })
+      );
+
+      const mockError = {
+        code: 1,
+        message: "User denied Geolocation",
+      };
+      mockWatchPosition.mockImplementationOnce(errorHandler(mockError));
+
+      const { result } = renderHook(() => useGeolocation());
+
+      expect(result.current).toStrictEqual({
+        accuracy: null,
+        altitude: null,
+        altitudeAccuracy: null,
+        heading: null,
+        latitude: null,
+        longitude: null,
+        speed: null,
+        timestamp: null,
+        error: mockError,
+      });
+    });
+  });
+
+  describe("when geolocation is not supported", () => {
+    beforeEach(() => {
+      global.navigator.geolocation = undefined;
+    });
+
+    it("returns no data", () => {
+      const { result } = renderHook(() => useGeolocation());
+
+      expect(result.current).toStrictEqual({
+        accuracy: null,
+        altitude: null,
+        altitudeAccuracy: null,
+        heading: null,
+        latitude: null,
+        longitude: null,
+        speed: null,
+        timestamp: null,
+        error: null,
+      });
     });
   });
 });
