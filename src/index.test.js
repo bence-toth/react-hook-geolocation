0e10ed23fff47cf9a03822c911d71eb0c8f43fa3
Bence A. Tóth <tothab@gmail.com>
2022-08-01 22:48:03 +0200

    Added test for watcher

--- src/index.test.js
+++ src/index.test.js
@@ -28,23 +28,65 @@ describe("useGeolocation", () => {
     mockGetCurrentPosition.mockImplementationOnce((success) =>
       Promise.resolve(
         success({
-          coords: {
-            latitude: 12.3456789,
-            longitude: 34.5678912,
-            altitude: null,
-            accuracy: 12.345,
-            altitudeAccuracy: null,
-            heading: null,
-            speed: null,
-          },
+          coords: mockCoordinates,
         })
       )
     );
+
     const { result } = renderHook(() => useGeolocation());
 
     expect(result.current).toStrictEqual({
       ...mockCoordinates,
       timestamp: undefined,
+      error: null,
+    });
+  });
+
+  it("updates geolocation from watcher", () => {
+    const mockCoordinatesFirst = {
+      latitude: 12.3456789,
+      longitude: 34.5678912,
+      altitude: null,
+      accuracy: 12.345,
+      altitudeAccuracy: null,
+      heading: null,
+      speed: null,
+    };
+
+    mockGetCurrentPosition.mockImplementationOnce((updateCoordinates) =>
+      Promise.resolve(
+        updateCoordinates({
+          coords: mockCoordinatesFirst,
+        })
+      )
+    );
+
+    const mockCoordinatesSecond = {
+      latitude: 23.4567891,
+      longitude: 45.6789123,
+      altitude: null,
+      accuracy: 2.3456,
+      altitudeAccuracy: null,
+      heading: null,
+      speed: null,
+    };
+
+    const mockTimestamp = 1659386826480;
+
+    mockWatchPosition.mockImplementationOnce((updateCoordinates) =>
+      Promise.resolve(
+        updateCoordinates({
+          coords: mockCoordinatesSecond,
+          timestamp: mockTimestamp,
+        })
+      )
+    );
+
+    const { result } = renderHook(() => useGeolocation());
+
+    expect(result.current).toStrictEqual({
+      ...mockCoordinatesSecond,
+      timestamp: mockTimestamp,
       error: null,
     });
   });
